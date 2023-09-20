b50ce1eb803e8c0ce24e67cbccc3c7ac336ccf77
jungjung <73343708+dzungmv@users.noreply.github.com>
2023-09-20 21:23:25 +0700

    Update utils.ts

--- src/libs/validations/utils.ts
+++ src/libs/validations/utils.ts
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import type { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { twMerge } from "tailwind-merge"
 
 export default function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
